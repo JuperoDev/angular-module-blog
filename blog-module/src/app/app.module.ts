d67f3123f3695d8ef0eafc196d0b702186be396d
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-10-23 16:40:11 +0200

    refactor: blog post checkpoint 2/3

--- blog-module/src/app/app.module.ts
+++ blog-module/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { FormsModule } from '@angular/forms';
 // Components
 import { BlogEntriesComponent } from './blog-entries/blog-entries.component';
 import { BlogEntryComponent } from './blog-entries/blog-entry/blog-entry.component';
-import { BlogDetailComponent } from './blog-post/blog-detail.component';
+
 import { BlogCategoriesComponent } from './blog-categories/blog-categories.component';
 import { BlogCategoryResultComponent } from './blog-category-result/blog-category-result.component';
 import { CosaComponent } from './cosa/cosa.component';
@@ -21,13 +21,14 @@ import { DeleteButtonComponent } from './blog-entries/blog-detail/delete-button/
 import { BlogAddButtonComponent } from './blog-functionality/blog-add-button/blog-add-button.component';
 import { BlogDeleteButtonComponent } from './blog-functionality/blog-delete-button/blog-delete-button.component';
 import { BlogAddPostComponent } from './blog-functionality/blog-add-post/blog-add-post.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogEntriesComponent,
     BlogEntryComponent,
-    BlogDetailComponent,
+
     BlogCategoriesComponent,
     BlogCategoryResultComponent,
     BlogAddPostComponent,
@@ -36,7 +37,8 @@ import { BlogAddPostComponent } from './blog-functionality/blog-add-post/blog-ad
 
     // refactor 
     BlogAddButtonComponent,
-    BlogDeleteButtonComponent
+    BlogDeleteButtonComponent,
+    BlogPostComponent,
   ],
   imports: [
     BrowserModule,
--- blog-module/src/app/app.routes.ts
+++ blog-module/src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogEntriesComponent } from './blog-entries/blog-entries.component';
-import { BlogDetailComponent } from './blog-post/blog-detail.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
 import { BlogCategoryResultComponent } from './blog-category-result/blog-category-result.component';  
 import { BlogAddPostComponent } from './blog-functionality/blog-add-post/blog-add-post.component';
 import { CosaComponent } from './cosa/cosa.component';
 
 const routes: Routes = [
   { path: '', component: BlogEntriesComponent },                             // Main page with all blog entries
-  { path: ':id', component: BlogDetailComponent },                          // Detailed blog post view
+  { path: ':id', component: BlogPostComponent },                          // Detailed blog post view
   { path: 'category/:category', component: BlogCategoryResultComponent },  // Category-specific page
   { path: 'add/post', component: BlogAddPostComponent },  // add post
   { path: 'posting/cosa', component: CosaComponent }
