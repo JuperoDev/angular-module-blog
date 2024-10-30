eb752df2a6c1ee17a88aab5692589ed65703e5ee
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-10-30 12:42:13 +0100

    milestone code is safe

--- blog-module/src/app/blog-post/blog-post.component.ts
+++ blog-module/src/app/blog-post/blog-post.component.ts
@@ -1,22 +1,17 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-blog-detail',
   templateUrl: './blog-post.component.html',
   styleUrls: ['./blog-post.component.scss']
 })
-export class BlogPostComponent implements OnInit, OnDestroy {
+export class BlogPostComponent implements OnInit {
   articleId: string = '';
   article: any;
-  private subscription: Subscription | null = null;
 
-  // Usar inject para inyectar dependencias
-  private route = inject(ActivatedRoute);
-  private http = inject(HttpClient);
-  private router = inject(Router);
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
     this.articleId = this.route.snapshot.paramMap.get('id')!;
@@ -25,7 +20,7 @@ export class BlogPostComponent implements OnInit, OnDestroy {
 
   fetchArticleDetails(id: string): void {
     const url = `https://firestore.googleapis.com/v1/projects/blog-a2581/databases/(default)/documents/articles/${id}`;
-    this.subscription = this.http.get<any>(url).subscribe({
+    this.http.get<any>(url).subscribe({
       next: (response) => {
         this.article = response;
       },
@@ -37,12 +32,6 @@ export class BlogPostComponent implements OnInit, OnDestroy {
 
   // Go home
   goBack(): void {
-    this.router.navigate(['']);  // Navegar a la ruta raíz
-  }
-
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+    this.router.navigate(['']);  // Navigate to root path
   }
 }
