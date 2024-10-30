d5859ae0f85665216df7a8af01a0d85ba6db5a11
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-10-30 10:52:00 +0100

    chore: delete unsuded component "cosa"

--- blog-module/src/app/app.routes.ts
+++ blog-module/src/app/app.routes.ts
@@ -4,14 +4,12 @@ import { BlogEntriesComponent } from './blog-entries/blog-entries.component';
 import { BlogPostComponent } from './blog-post/blog-post.component';
 import { BlogCategoryResultComponent } from './blog-category-result/blog-category-result.component';  
 import { BlogAddPostComponent } from './blog-functionality/blog-add-post/blog-add-post.component';
-import { CosaComponent } from './cosa/cosa.component';
 
 const routes: Routes = [
   { path: '', component: BlogEntriesComponent },                             // Main page with all blog entries
   { path: ':id', component: BlogPostComponent },                          // Detailed blog post view
   { path: 'category/:category', component: BlogCategoryResultComponent },  // Category-specific page
-  { path: 'add/post', component: BlogAddPostComponent },  // add post
-  { path: 'posting/cosa', component: CosaComponent }
+  { path: 'add/post', component: BlogAddPostComponent } // add post
 ];
 
 @NgModule({
