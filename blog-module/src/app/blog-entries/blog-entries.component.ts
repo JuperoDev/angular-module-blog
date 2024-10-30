6c9a0b70dbf47870377cc8db28dd2584c44d2eeb
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-10-30 15:59:18 +0100

    Update blog-entries.component.ts

--- blog-module/src/app/blog-entries/blog-entries.component.ts
+++ blog-module/src/app/blog-entries/blog-entries.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
-
+import { FirestoreResponse, FirestoreDocument, Article } from '../../models/article.model';
 @Component({
   selector: 'app-blog-entries',
   templateUrl: './blog-entries.component.html',
@@ -19,7 +19,7 @@ export class BlogEntriesComponent implements OnInit, OnDestroy {
 
   fetchArticles(): void {
     const url = 'https://firestore.googleapis.com/v1/projects/blog-a2581/databases/(default)/documents/articles';
-    this.subscription = this.http.get<{ documents: any[] }>(url).subscribe({
+    this.subscription = this.http.get<{ documents: FirestoreDocument[] }>(url).subscribe({
       next: (response) => {
         this.articles = response.documents;
       },
